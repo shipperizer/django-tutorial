951054ed2ef57f72d2b0dd60782d66736df5cecc
shipperizer <alexcabb@gmail.com>
2015-06-25 17:19:53 +0100

    i forgot how easy was using django views

--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from polls.models import Question
+
+
 def index(request):
-    return HttpResponse("yo")
+    questions = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'questions': questions})
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
